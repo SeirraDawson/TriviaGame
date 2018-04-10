ff0b2ccf1a1ab1b0571cac46e34f255484782462
S. Dawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-04-10 18:37:56 -0400

    Initial updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,92 @@
+// Trivia Questions
+var triviaQuestions = [{
+        question: "What is Tommy's job?",
+        mcAnswers: ["Unemployed", "Firefighter", "Pimp", "Community Center Counselor"],
+        answerIndex: 3
+        // While it was a running gag that the other characters believed Tommy Strawn did not have a job,
+        // there were clues throughout the series that Tommy was a counselor at a Boys & Girls Club.
+
+},{
+
+        question: "Which phrase did the Martin show popularize?",
+        mcAnswers: ["Talk to the hand", "Scary J. Blige", "Don't you know no good?", "You kill'n me smalls!"],
+        answerIndex: 0
+        // "Martin" is recognized for popularizing the phrase, "Talk to the hand."
+},{
+
+        question: "Which rapper did not appear on “Martin”?",
+        mcAnswers: ["The Notorious B.I.G", "MC Hammer", "Tupac", "Method Man"],
+        answerIndex: 3
+        // A number of hip-hop artists from the '90s. However, Tupac was not one of them.
+},{
+        question: "What city did “Martin” take place?",
+        mcAnswers: ["Atlanta", "Detroit", "Chicago", "Philadelphia"],
+        answerIndex: 1
+        // While "Martin" what filmed in Los Angeles, it took place in Detroit.
+},{
+        question: "Who did Martin pick on the most?",
+        mcAnswers: ["Gina", "Tommy", "Bruh Man", "Pam"],
+        answerIndex: 3
+},{
+        question: "What floor did Bruh Man living on in the apartment complex?",
+        mcAnswers: ["2nd floor", "5th floor", "3rd floor", "1st floor"],
+        answerIndex: 1
+},{
+        question: "Who sang at Martin and Gina’s engagement?",
+        mcAnswers: ["Brian McKnight", "Babyface", "Stevie Wonder", "Jodeci"],
+        answerIndex: 0
+},{
+        question: "What is Gina’s father’s profession?",
+        mcAnswers: ["Judge", "Lawyer", "Chiropractor", "Family Physician"],
+        answerIndex: 2
+},{
+        question: "Martin and Gina are rich. They have won the lottery or so they think. Martin gives gifts to all his friends. What does he give Pam?",
+        mcAnswers: ["A $2000 back shaving job", "A $2000 pedicure", "A $2000 plastic surgery job", "A $2000 wax job"],
+        answerIndex: 3
+},{
+        question: "Martin went on a rent strike because his rent was increased and the landlord refused to fix any of the appliances around the apartment. How much was the increase?",
+        mcAnswers: ["10%", "5%", "$5.00", "$50.00"],
+        answerIndex: 2
+},{
+
+
+}];
+
+
+
+
+// Countdown timer
+var timer = 15;
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var unanswered = 0;
+
+
+
+
+// ----------- Game Functions are below ------------
+$(document).ready(function() {
+
+
+
+
+
+//on click of start button game begins timer countdown
+function countdown () {
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+});
